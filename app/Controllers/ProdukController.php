dce56fa60e0c266c17bf86b3fa89dbe48860b649
MUSA16 <musafadhlillah@gmail.com>
2026-06-01 20:22:12 +0700

    Capstone-8-CRUD

--- app/Controllers/ProdukController.php
+++ app/Controllers/ProdukController.php
@@ -3,12 +3,78 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ProductModel;
 
 class ProdukController extends BaseController
 {
+    protected $productModel;
+
+    function __construct()
+    {
+        $this->productModel = new ProductModel();
+    }
+
     public function index()
     {
-        return view('v_produk');
+        return view('Produk/index', [
+            'products' => $this->productModel->findAll()
+        ]);
     }
+
+    public function create()
+    {
+        $foto = $this->request->getFile('foto');
+        $namaFoto = '';
+
+        if ($foto && $foto->isValid() && ! $foto->hasMoved()) {
+            $namaFoto = $foto->getRandomName();
+            $foto->move(FCPATH . 'img', $namaFoto);
+        }
+
+        $this->productModel->insert([
+            'nama'   => $this->request->getPost('nama'),
+            'harga'  => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah'),
+            'foto'   => $namaFoto,
+        ]);
+        
+        return redirect()->to(base_url('produk'))->with('success', 'Data berhasil ditambahkan');
+    }
+    public function edit($id)
+{
+    $dataProduk = $this->productModel->find($id);
+
+    $dataForm = [
+        'nama' => $this->request->getPost('nama'),
+        'harga' => $this->request->getPost('harga'),
+        'jumlah' => $this->request->getPost('jumlah') 
+    ];
+
+    if ($this->request->getPost('check') == 1) {
+        if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+            unlink("img/" . $dataProduk['foto']);
+        }
+
+        $dataFoto = $this->request->getFile('foto');
+
+        if ($dataFoto->isValid()) {
+            $fileName = $dataFoto->getRandomName();
+            $dataFoto->move('img/', $fileName);
+            
+            $dataForm['foto'] = $fileName;
+        }
+    }
+
+    $this->productModel->update($id, $dataForm);
+
+    return redirect('produk')->with('success', 'Data Berhasil Diubah');
+}
+
+public function delete($id)
+{
+    $dataProduk = $this->productModel->find($id);
+    $this->productModel->delete($id);
+
+    return redirect('produk')->with('success', 'Data Berhasil Dihapus');
+}
 }
--- app/Views/v_home.php
+++ app/Views/v_home.php
@@ -2,10 +2,17 @@
 <?= $this->section('content') ?>
 
               <!-- Table with stripped rows -->
-              <?php foreach ($products as $key => $item) : ?>         
-        <img src="<?= base_url() . "img/" . $item['foto'] ?>" width="100"><br>
-        <?= $item['nama'] ?><br>
-        <?= $item['harga'] ?><br>         
-<?php endforeach ?> 
+            <div class="row">
+    <?php foreach ($products as $key => $item) : ?>         
+            <div class="col-lg-6">
+                <div class="card">
+                    <div class="card-body">
+                        <img src="<?= base_url() . "img/" . $item['foto'] ?>" alt="..." width="50%">
+                        <h5 class="card-title"><?= $item['nama'] ?><br><?= $item['harga'] ?></h5>
+                    </div>
+                </div>
+            </div> 
+    <?php endforeach ?> 
+</div>
               <!-- End Table with stripped rows -->
                <?= $this->endSection() ?>
